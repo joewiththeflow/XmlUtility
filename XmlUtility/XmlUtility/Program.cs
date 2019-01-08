ea48e7e0ab2cc1e2d3d7a87af3b8ae56ee8ea77b
Joe <joewiththeflow@hotmail.co.uk>
2019-01-08 21:03:08 +0000

    Create OldFiles and NewFiles folders if they don't exist

--- XmlUtility/XmlUtility/Program.cs
+++ XmlUtility/XmlUtility/Program.cs
@@ -4,18 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//The aim of this program is to help with XML files going forward.
+//For example, my first task is to create a method which will rearrange attributes for a particular key
+//  e.g. a file which has key/value attributes for various <add> nodes, but you want a way to easily call the 
+//SwitchAttributesForKey method, which will switch attributes from key/value to value/key in the xml
+// <add key="key1" value="value1" /> becomes <add value="value1" key="key2" />
+
 namespace XmlUtility
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            if (!(args == null) && args.Length == 4)
+            {
+                switch (args[0])
+                {
+                    case "SwitchAttributesForKey":
+                        SwitchAttributesForKey.SwitchAttributes(args[1], args[2], args[3]);
+                        break;
+                }
+            }
         }
     }
 }
---
+++ XmlUtility/XmlUtility/SwitchAttributesForKey.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlUtility
+{
+    public class SwitchAttributesForKey
+    {
+        //My first task is to create a method which will rearrange attributes for a particular key
+        //  e.g. a file which has key/value attributes for various <add> nodes, but you want a way to easily call the 
+        //SwitchAttributesForKey method, which will switch attributes from key/value to value/key in the xml
+        // <add key="key1" value="value1" /> becomes <add value="value1" key="key2" />
+
+        //we should have a folder called OldFiles and an another called NewFiles
+
+
+        public static void SwitchAttributes(string key, string firstAttribute, string secondAttribute)
+        {
+            string oldFilesDirectory = "../../OldFiles";
+            string newFilesDirectory = "../../NewFiles";
+
+            //create the OldFiles directory if it doesn't exist
+            System.IO.Directory.CreateDirectory(oldFilesDirectory);
+            //create the NewFiles directory if it doesn't exist
+            System.IO.Directory.CreateDirectory(newFilesDirectory);
+
+        }
+    }
+}
