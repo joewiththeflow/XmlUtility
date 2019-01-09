c348d63e7c4db0d17795cd46a439ea79ddbd8ced
Joe <joewiththeflow@hotmail.co.uk>
2019-01-09 19:13:24 +0000

    Can take in an old xml file and write it out as a new file

--- XmlUtility/XmlUtility/SwitchAttributesForKey.cs
+++ XmlUtility/XmlUtility/SwitchAttributesForKey.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace XmlUtility
 {
@@ -15,17 +17,54 @@ namespace XmlUtility
 
         //we should have a folder called OldFiles and an another called NewFiles
 
+        private static void CreateFolder(string path)
+        {
+            //create the directory if it doesn't exist
+            System.IO.Directory.CreateDirectory(path);
+        }
 
         public static void SwitchAttributes(string key, string firstAttribute, string secondAttribute)
         {
-            string oldFilesDirectory = "../../OldFiles";
-            string newFilesDirectory = "../../NewFiles";
+            string workingDirectory = "../../";
+            string oldFilesDirectory = workingDirectory + "OldFiles/";
+            string newFilesDirectory = workingDirectory + "NewFiles/";
 
             //create the OldFiles directory if it doesn't exist
-            System.IO.Directory.CreateDirectory(oldFilesDirectory);
+            CreateFolder(oldFilesDirectory);
             //create the NewFiles directory if it doesn't exist
-            System.IO.Directory.CreateDirectory(newFilesDirectory);
+            CreateFolder(newFilesDirectory);
 
+            //Get a list of the fles in the OldFiles directory
+            FileInfo[] oldFiles = new DirectoryInfo(oldFilesDirectory).GetFiles();
+
+            //loop through each file
+            foreach(FileInfo fileInfo in oldFiles)
+            {
+                //load the original xml file into memory
+                XmlDocument oldFile = new XmlDocument();
+                oldFile.Load(fileInfo.FullName);
+
+                //I think you want to load the new file here as well
+                XmlDocument newFile = new XmlDocument();
+                newFile.Load(fileInfo.FullName);
+
+                //access the document element of the file
+                XmlNode currentNode = oldFile.DocumentElement;
+                //get a list of all the nodes in the xml file
+                XmlNodeList listOfNodes = oldFile.SelectNodes("*");
+
+                //write this out to the new file
+                using (var writer = System.IO.File.CreateText(newFilesDirectory + fileInfo.Name))
+                {
+                    writer.WriteLine(currentNode);
+                    foreach (XmlNode node in listOfNodes)
+                    {
+                        writer.WriteLine(node.OuterXml);   // InnerXml to get only the content
+                    }
+                }
+
+       
+            }
         }
     }
 }
